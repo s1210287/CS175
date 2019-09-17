a063d8ba803d89170509637767176cc6205b697d
Kathryn Schauer <s1210287@monmouth.edu>
2019-09-16 20:56:55 -0400

    variables created

---
+++ HousePainting/src/PaintCost.java
@@ -0,0 +1,26 @@
+
+public class PaintCost {
+
+	public static void main(String[] args) {
+		/*
+		 *This program will calculate the cost to paint a house 
+		 */
+		double houselengthFeet;
+		double houseWidthFeet;
+		double houseHeightFeet;
+		double windowLengthFeet;
+		double windowWidthFeet;
+		double windowNumberFeet;
+		double doorLengthFeet;
+		double doorWidthFeet;
+		double doorNumberFeet;
+		
+		//example
+		int houseLengthFeet=20;
+		double inches = 6;
+		System.out.println(houseLengthFeet + inches/12);
+		
+
+	}
+
+}
