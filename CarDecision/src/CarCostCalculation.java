077927e57a2d4de278dc1fdf6ae4df02fa2bc1c4
Kathryn Schauer <s1210287@monmouth.edu>
2019-09-25 21:06:37 -0400

    Car

---
+++ CarDecision/src/CarCostCalculation.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class CarCostCalculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		//Regular Car
+		//28000
+		System.out.print("Enter cost of regular car: ");
+		double CostOfRegularCar = in.nextDouble();
+		//20
+		System.out.print("Enter miles per gallon on regular car: ");
+		double Mpgr = in.nextDouble();
+		
+		//Hybrid Car
+		//31000
+		System.out.print("Enter cost of hybrid car: ");
+		double CostOfHybridCar = in.nextDouble();
+		//30
+		System.out.print("Enter miles per gallon on hybrid car: ");
+		double Mpgh = in.nextDouble();
+		
+		//Other info
+		//15000
+		System.out.print("Enter miles traveled in a year: ");
+		double MilesTraveledYear = in.nextDouble();
+		//2.50
+		System.out.print("Enter cost per gallon of gas: ");
+		double CostPerGallonGas = in.nextDouble();
+		
+		//Equations 
+	}
+
+}
