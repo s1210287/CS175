474bb283574cea06674f5d2065cb13878444cfda
Kathryn Schauer <s1210287@monmouth.edu>
2019-10-28 20:54:28 -0400

    yas

--- GroceryDiscount/src/groceryDiscount/groceryDiscountVer2.java
+++ GroceryDiscount/src/groceryDiscount/groceryDiscountVer2.java
@@ -1,5 +1,6 @@
 package groceryDiscount;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class groceryDiscountVer2 {
@@ -7,19 +8,70 @@ public class groceryDiscountVer2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		double total = 0;
-		int count = 0;
-		//System.out.print("Enter amount of 'E' to end:");
-		System.out.print("Enter grocery amount, any other response to end:");
+		NumberFormat formatter = NumberFormat.getCurrencyInstance();
+		
+		System.out.print("Enter grocery amount, any other response to end: ");
 		while (in.hasNextDouble())
 		{
-			double input = in.nextDouble();
-			System.out.print("Enter amount:");
-			if(input>=10 && input <=1000)
-			{
-				System.out.print("Enter grocery amount, any other response to end: ");
-			}
+			double moneySpent = in.nextDouble();
 			
+			if(moneySpent>=10 && moneySpent <=1000)
+				
+			{
+			if (moneySpent <= 60.0) 
+				{
+				double discount = .08;
+				System.out.println("You win a discount coupon of: " + formatter.format(moneySpent*discount) + ".(" + "8% of your purchase)");
+				}
+			else if (moneySpent <= 150)
+				{
+				double discount = .10;
+				System.out.println("You win a discount coupon of: " + formatter.format(moneySpent*discount)+ ".(" + "10% of your purchase)");  
+				}
+			else if (moneySpent <= 210)
+				{
+				double discount = .12;
+				System.out.println("You win a discount coupon of: " + formatter.format(moneySpent*discount) + ".(" + "12% of your purchase)");
+				}
+			else if (210 < moneySpent)
+				{
+				double discount = .14;
+				System.out.println("You win a discount coupon of: " + formatter.format(moneySpent*discount) + ".(" + "14% of your purchase)");
+				
+				} 
+		
+			}
+			else {System.out.println("Amount too high or too low please re enter: ");}
+			System.out.print("Enter grocery amount, any other response to end: ");
+			} 
+		
+		}
+}
+
+
+
+
+	
+
+
+
+
+	
+		
+			
+		
+	
+
+		
+	
+
+	
+
+	
+		
+	
+	
+
 			
 			//{total = total + input;
 			//count++;}
@@ -37,7 +89,7 @@ public class groceryDiscountVer2 {
 			
 				
 		
-		}
-	}
+		
+		
 
-}
+	
