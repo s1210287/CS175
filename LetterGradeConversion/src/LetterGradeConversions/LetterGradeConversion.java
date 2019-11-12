e1956839bd81b2271fe59d488692e3be41ce6187
Kathryn Schauer <s1210287@monmouth.edu>
2019-11-12 13:00:40 -0500

    yup

--- LetterGradeConversion/src/LetterGradeConversions/LetterGradeConversion.java
+++ LetterGradeConversion/src/LetterGradeConversions/LetterGradeConversion.java
@@ -1,10 +1,85 @@
-package LetterGradeConversions;
+	package LetterGradeConversions;
+	
+	import java.util.Scanner;
 
-public class LetterGradeConversion {
-
+	public class LetterGradeConversion {
+		
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		{
+		String grade;
+		double loop = 0;
+		double numericValue = 5;
+		
+		while (loop == 0) {
+			System.out.println("Enter a letter grade or 'Q' to quit the program: ");
+			grade = in.next();
+		
+		if (grade.contentEquals("Q"))
+			loop =2;
+		
+		if (loop == 2)
+			break;
+		
+		if (grade.contentEquals("A+") || grade.contentEquals("A"))
+		{
+		numericValue = 4.0;
+		}
+		else if (grade.contentEquals("A-"))
+		{
+		numericValue = 3.7;
+		}
+		else if (grade.contentEquals("B+"))
+		{
+		numericValue = 3.3;
+		}
+		else if (grade.contentEquals("B"))
+		{
+		numericValue = 3.0;
+		}
+		else if (grade.contentEquals("B-"))
+		{
+		numericValue = 2.7;
+		}
+		else if (grade.contentEquals("C+"))
+		{
+		numericValue = 2.3;
+		}
+		else if (grade.contentEquals("C"))
+		{
+		numericValue = 2.0;
+		}
+		else if (grade.contentEquals("C-"))
+		{
+		numericValue = 1.7;
+		}
+		else if (grade.contentEquals("D+"))
+		{
+		numericValue = 1.3;
+		}
+		else if (grade.contentEquals("D"))
+		{
+		numericValue = 1.0;
+		}
+		else if (grade.contentEquals("D-"))
+		{
+		numericValue = 0.7;
+		}
+		else if (grade.contentEquals("F"))
+		{
+		numericValue = 0.0;
+		}
+		System.out.println("The numeric value is " + numericValue);
+		
+		}
 	}
-
+    
+	
+	
+	
+	
+    
+	}
 }
--- LetterGradeConversion/src/LetterGradeConversions/LetterGradeTest.java
+++ LetterGradeConversion/src/LetterGradeConversions/LetterGradeTest.java
@@ -1,5 +1,5 @@
 package LetterGradeConversions;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import java.io.*;
@@ -11,69 +11,68 @@ public class LetterGradeTest {
 		
 		Scanner in = new Scanner(System.in);
 		
-		{
-		String grade;
-		double loop = 0;
-		double numericValue = 5;
+		String english1 = "Make:Toyota, Model:Prius, Cost:31000, MPG:30";
+		String nutrition = "CreditHours:3, Grade:A, GradePoints:4";
+		String fYS = "CreditHours:#, Grade: , GradePoints: ";
+		String it = "CreditHours:#, Grade: , GradePoints: ";
+		String westCiv = "CreditHours:#, Grade: , GradePoints: ";
+		String english2 = "CreditHours:#, Grade: , GradePoints: ";
+		String psychology = "CreditHours:#, Grade: , GradePoints: ";
+		String anthropology = "CreditHours:#, Grade: , GradePoints: ";
+		String mathSS = "CreditHours:#, Grade: , GradePoints: ";
+		String preCalc = "CreditHours:#, Grade: , GradePoints: ";
+		String discreteM = "CreditHours:#, Grade: , GradePoints: ";
+		String introCS = "CreditHours:#, Grade: , GradePoints: ";
+		String CS = "CreditHours:#, Grade: , GradePoints: ";
+		String CSL = "CreditHours:#, Grade: , GradePoints: ";
 		
-		while (loop == 0) {
-			System.out.println("Enter a letter grade or 'Q' to quit the program: ");
-			grade = in.next();
+		String [] english01 = english1.split(",");
 		
-		if (grade.contentEquals("Q"))
-			loop =2;
+		String [] nutritionn = nutrition.split(",");
 		
-		if (loop == 2)
-			break;
+		String [] fYSS = fYS.split(",");
 		
-		if (grade.contentEquals("A+") || grade.contentEquals("A"))
-		{
-		numericValue = 4.0;
+		String [] itt = it.split(",");
+		
+		String [] westCivv = westCiv.split(",");
+		
+		String [] english03 = english2.split(",");
+		
+		String [] psychologyy =psychology.split(",");
+		
+		String [] anthropologyy = anthropology.split(",");
+		
+		String [] mathSSS = mathSS.split(",");
+		
+		String [] preCalcc = preCalc.split(",");
+		
+		String [] discretee = discreteM.split(",");
+		
+		String [] introCSS = introCS.split(",");
+		
+		String [] CSS = CS.split(",");
+		
+		String [] CSLL = CSL.split(",");
+		
+		
+		String crouse = "English";
+		int cred = 3;
+		String grade = "B+";
+		ArrayList<String>courses = new ArrayList<String>();
+		courses.add(courses+","+cred+","+grade);
+		courses.add(courses+","+cred+","+grade);
+		for(String element:courses) {
+			System.out.println(element);
 		}
-		else if (grade.contentEquals("A-"))
-		{
-		numericValue = 3.7;
-		}
-		else if (grade.contentEquals("B+"))
-		{
-		numericValue = 3.3;
-		}
-		else if (grade.contentEquals("B"))
-		{
-		numericValue = 3.0;
-		}
-		else if (grade.contentEquals("B-"))
-		{
-		numericValue = 2.7;
-		}
-		else if (grade.contentEquals("C+"))
-		{
-		numericValue = 2.3;
-		}
-		else if (grade.contentEquals("C"))
-		{
-		numericValue = 2.0;
-		}
-		else if (grade.contentEquals("C-"))
-		{
-		numericValue = 1.7;
-		}
-		else if (grade.contentEquals("D+"))
-		{
-		numericValue = 1.3;
-		}
-		else if (grade.contentEquals("D"))
-		{
-		numericValue = 1.0;
-		}
-		else if (grade.contentEquals("F"))
-		{
-		numericValue = 5;
-		}
-		System.out.println(numericValue);
-		System.out.println(" ");
-		}
-	}
+		
+		
+		//course
+		//credit hours
+		//grade
+		//grade points
+		
+		
+		
 			
 	}
 }
